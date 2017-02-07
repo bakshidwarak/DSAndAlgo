1d1d82049210b3981acbf9a0abde848004b57dac
Dwarak <dwaraknath.bakshi@gmail.com>
2017-02-07 09:48:55 -0800

    NQueens

--- src/recursion/assignment/NQueens/NQueens.java
+++ src/recursion/assignment/NQueens/NQueens.java
@@ -1,5 +1,37 @@
 package recursion.assignment.NQueens;
 
+import java.util.Arrays;
+
 public class NQueens {
+	
+	public static void main(String[] args) {
+		char[][] chessBoard= new char[5][5];
+		for(int i=0; i<chessBoard.length;i++){
+			chessBoard[i]= new char[chessBoard.length];
+			Arrays.fill(chessBoard[i], 'X');
+		}
+		solveNQueensUsingBackTracing(chessBoard,5);
+		
+	}
+
+	private static void solveNQueensUsingBackTracing(char[][] chessBoard, int i) {
+		
+		int col=0; int row=0;
+		
+		placeQueens(chessBoard,col,row);
+		
+	}
+
+	private static void placeQueens(char[][] chessBoard, int col, int row) {
+		
+		for(int column=col;column<chessBoard.length; column++){
+			for(int currentRow=row; row<chessBoard.length; row++){
+				if(chessBoard[column][row]=='X'){
+					
+				}
+			}
+		}
+		
+	}
 
 }
