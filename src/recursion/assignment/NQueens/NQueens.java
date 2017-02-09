e5e9f6d55b3e872ced5773d01aa644676d0f3b42
Dwarak <dwaraknath.bakshi@gmail.com>
2017-02-09 05:49:33 -0800

    Hardening N Queens

--- src/recursion/assignment/NQueens/NQueens.java
+++ src/recursion/assignment/NQueens/NQueens.java
@@ -3,77 +3,127 @@ package recursion.assignment.NQueens;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 
+/**
+ * The n-queens puzzle is the problem of placing n queens on an n×n chessboard
+ * such that no two queens attack each other.
+ * 
+ * 
+ * 
+ * Given an integer n, return all distinct solutions to the n-queens puzzle.
+ * 
+ * Each solution contains a distinct board configuration of the n-queens'
+ * placement, where 'Q' and '.' both indicate a queen and an empty space
+ * respectively.
+ * 
+ * For example, There exist two distinct solutions to the 4-queens puzzle:
+ * 
+ * @author dwaraknathbs
+ *
+ */
 public class NQueens {
 
 	public static void main(String[] args) {
-		int n = 5;
+		Scanner scanner = new Scanner(System.in);
+		int n = scanner.nextInt();
+		scanner.close();
 		char[][] chessBoard = new char[n][n];
 		for (int i = 0; i < chessBoard.length; i++) {
 			chessBoard[i] = new char[chessBoard.length];
 			Arrays.fill(chessBoard[i], 'X');
 		}
 
+		System.out.println("Solutions using back tracking");
 		List<char[][]> solutions = solveNQueensUsingBackTracing(chessBoard, n);
 		solutions.stream().forEach(sol -> printSolution(sol));
-		
-		 solutions =solveNQueensUsingPermutations(n);
-		 solutions.stream().forEach(sol -> printSolution(sol));
+		System.out.println("Solutions using permutations");
+		solutions = solveNQueensUsingPermutations(n);
+		solutions.stream().forEach(sol -> printSolution(sol));
 
 	}
 
+	/**
+	 * Key idea is to think in terms of output. We do not need a 2D array to
+	 * denote the positions , instead it could be a 1D array with indices
+	 * denoting the rows and the values in arrays denoting the columns. Once
+	 * this insight is clear different positions of the queens in the baord is
+	 * just the different permutations of the numbers 1..n
+	 * 
+	 * @param n
+	 * @return
+	 */
 	private static List<char[][]> solveNQueensUsingPermutations(int n) {
-		int[] rows= new int[n+1];
-		List<char[][]> solutions=new ArrayList<>();
-		 nQueens(rows,1,n,solutions);
-		 return solutions;
+		int[] rows = new int[n];
+		for (int i = 0; i < n; i++) {
+			rows[i] = i + 1;
+		}
+		List<char[][]> solutions = new ArrayList<>();
+		nQueens(rows, 0, n, solutions);
+		return solutions;
 	}
 
+	/**
+	 * Contract: I get a position decided till the index, I will check for
+	 * permutations from my index and revert it to the state I was given in
+	 * 
+	 * @param rows
+	 * @param index
+	 * @param n
+	 * @param solutions
+	 */
 	private static void nQueens(int[] rows, int index, int n, List<char[][]> solutions) {
-		
-		if(index==rows.length){
-			
-				char[][] sol= new char[n][n];
-				for(int i=1; i<rows.length;i++){
-					int colIndex=rows[i];
-					sol[colIndex]=new char[rows.length];
-					sol[colIndex][i]='Q';
-				}
-				
-				solutions.add(sol);
-			
-			
+
+		if (index == rows.length) {
+
+			char[][] sol = new char[n][n];
+			for (int i = 0; i < rows.length; i++) {
+				int colIndex = rows[i];
+				sol[colIndex - 1] = new char[rows.length];
+				sol[colIndex - 1][i] = 'Q';
+			}
+
+			solutions.add(sol);
+
 		}
 
-		
-		for(int k=index;k<rows.length;k++){
-			swap(rows,index,k);
-			if(isProperPosition(rows,index)){
-			nQueens(rows,index+1,n,solutions);
+		for (int k = index; k < rows.length; k++) {
+			swap(rows, index, k);
+			if (isProperPosition(rows, index)) {
+				nQueens(rows, index + 1, n, solutions);
 			}
-			swap(rows,k,index);
+			swap(rows, index, k);
 		}
 	}
 
+	/**
+	 * Given a position as a 1D array determines if the queen is threatening
+	 * each other.
+	 * 
+	 * @param rows
+	 * @param index
+	 * @return
+	 */
 	private static boolean isProperPosition(int[] rows, int index) {
-		for(int i=1; i<index;i++){
-			int col=rows[i];
-			int col2=rows[index];
-			int r1=index;
-			int r2=i;
-			if(Math.abs(r1-r2)==Math.abs(col-col2))
+		for (int i = 0; i < index; i++) {
+			int col = rows[i];
+			int col2 = rows[index];
+			int r1 = index;
+			int r2 = i;
+			if (Math.abs(r1 - r2) == Math.abs(col - col2))
 				return false;
-			
+
 		}
 		return true;
 	}
 
 	private static void swap(int[] arr, int s, int k) {
-		int temp=arr[s];
-		arr[s]=arr[k];
-		arr[k]=temp;
-		
+		int temp = arr[s];
+		arr[s] = arr[k];
+		arr[k] = temp;
+
 	}
+
 	private static void printSolution(char[][] sol) {
 		for (int i = 0; i < sol.length; i++) {
 			for (int j = 0; j < sol.length; j++) {
@@ -115,8 +165,6 @@ public class NQueens {
 	private static void placeQueens(char[][] chessBoard, int row, int n, List<char[][]> solution) {
 
 		if (n == 0) {
-			System.out.println("Solution found");
-
 			solution.add(copy(chessBoard));
 			return;
 		}
