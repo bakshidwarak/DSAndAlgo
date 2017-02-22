512af6a80c517a76c24009a90e759e12cfab453e
Dwarak <dwaraknath.bakshi@gmail.com>
2017-02-22 12:50:50 -0800

    Word break

--- src/dp/assignment/wordbreak/WordBreak.java
+++ src/dp/assignment/wordbreak/WordBreak.java
@@ -2,9 +2,29 @@ package dp.assignment.wordbreak;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
+/**
+ * Word Break Given an input string and a dictionary of words, segment the input
+ * string into a space-separated sequence of dictionary words if possible.
+ * 
+ * For example, if the input string is "applepie" and dictionary contains a
+ * standard set of English words, then we would return the string "apple pie" as
+ * output.
+ * 
+ * If you can find multiple arrangements, then print all such combinations.
+ * 
+ * 
+ * Solution: http://www.programcreek.com/2014/03/leetcode-word-break-ii-java/
+ * 
+ * 
+ * @author dwaraknathbs
+ *
+ */
 public class WordBreak {
 	public static void main(String[] args) throws IOException {
 		Scanner in = new Scanner(System.in);
@@ -39,27 +59,84 @@ public class WordBreak {
 	}
 
 	private static String[] wordBreak(String word, String[] dictionary) {
-		String[][] brokenWords=new String[word.length()+1][word.length()+1];
-		wordBreakHelperRecursive(word,dictionary,brokenWords);
-		
-		return brokenWords[word.length()];
+
+		List<Integer>[] wordIndices = new List[word.length() + 1];
+		int firstIndex = wordBreakHelper(word, dictionary, wordIndices);
+
+		ArrayList<String> current = new ArrayList<>();
+		ArrayList<ArrayList<String>> brokenWords = new ArrayList<>();
+		dfsPrint(wordIndices, firstIndex, word, current, brokenWords);
+		String[] array = new String[brokenWords.size()];
+		int i = 0;
+		for (List<String> list : brokenWords) {
+			Collections.reverse(list);
+			String string = list.stream().collect(Collectors.joining(" "));
+			array[i++] = string;
+			//System.out.println(string);
+		}
+
+		Arrays.sort(array);
+		return array;
 	}
 
-	private static void wordBreakHelperRecursive(String word, String[] dictionary, String[][] brokenWords) {
-		for(int i=0; i<word.length();i++){
-			
-			if(isInDictionary(word.substring(0, i),dictionary)){
-				if(brokenWords[i]==null){
-					brokenWords[i]=new String[word.length()];
-					
-				}
+	private static void dfsPrint(List<Integer>[] wordIndices, int firstIndex, String word, ArrayList<String> current,
+			ArrayList<ArrayList<String>> brokenWords) {
+		if (firstIndex == -1) {
+			ArrayList<String> currentList = new ArrayList<>(current);
+			brokenWords.add(currentList);
+			return;
+
+		}
+		List<Integer> firstList = wordIndices[firstIndex];
+		if (firstList != null) {
+			for (int first : firstList) {
+				current.add(word.substring(first, firstIndex + 1));
+				dfsPrint(wordIndices, first - 1, word, current, brokenWords);
+				current.remove(current.size() - 1);
 			}
 		}
-		
+
+	}
+
+	/**
+	 * Basically creating a table of end index to the list of start indices of
+	 * words that are present in the dictionary
+	 * 
+	 * Then DFS the table from the last index that has a legit word till it gets
+	 * all words
+	 * 
+	 * @param word
+	 * @param dictionary
+	 * @param wordIndices
+	 * @return
+	 */
+	private static int wordBreakHelper(String word, String[] dictionary, List<Integer>[] wordIndices) {
+		int firstIndex = -1;
+		int end = word.length() - 1;
+		for (int i = end; i >= 0; i--)
+			for (int j = i; j >= 0; j--) {
+				if (isInDictionary(word.substring(j, i + 1), dictionary)) {
+					if (firstIndex == -1) {
+						firstIndex = i;
+					}
+					if (wordIndices[i] == null) {
+						List<Integer> numList = new ArrayList<>();
+						numList.add(j);
+						wordIndices[i] = numList;
+
+					} else {
+						wordIndices[i].add(j);
+					}
+				}
+
+			}
+
+		return firstIndex;
+
 	}
 
 	private static boolean isInDictionary(String word, String[] dictionary) {
-		// TODO Auto-generated method stub
-		return false;
+		return Arrays.stream(dictionary).filter(str -> str.equals(word)).findFirst().isPresent();
+
 	}
 }
