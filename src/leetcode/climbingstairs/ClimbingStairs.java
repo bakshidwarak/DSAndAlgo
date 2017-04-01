cfcdd57ca8c78dbec8d331914c6015a44651fee9
Dwarak <dwbs@paypal.com>
2017-03-31 23:36:17 -0700

    Leet code climbing stairs

---
+++ src/leetcode/climbingstairs/ClimbingStairs.java
@@ -0,0 +1,37 @@
+
+
+package leetcode.climbingstairs;
+
+/**
+ * 70. Climbing Stairs 
+ * 
+ * You are climbing a stair case. It takes n steps to reach
+ * to the top. Each time you can either climb 1 or 2 steps. In how many distinct
+ * ways can you climb to the top? Note: Given n will be a positive integer. Show
+ * Company Tags Show Tags
+ * 
+ * @author dwaraknathbs
+ */
+public class ClimbingStairs {
+
+    public static void main(String[] args) {
+        System.out.println(climbStairs(10));
+
+    }
+    
+    public static int climbStairs(int n) {
+        if(n==1) return 1;
+        if(n==2) return 2;
+        if(n==0) return 0;
+        int[] cache= new int[n+1];
+        cache[0]=0;
+        cache[1]=1;
+        cache[2]=2;
+    
+        for(int i=3; i<=n;i++){
+            cache[i]=cache[i-1]+cache[i-2];
+        }
+        return cache[n];
+    }
+
+}
