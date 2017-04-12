47b69295ac203d8b9c674dffa4d05f32503a6109
Dwarak <dwaraknath.bakshi@gmail.com>
2017-04-12 06:43:14 -0700

    Number of moves to make elements equal

---
+++ src/leetcode/minmovestomovearrayelements/MinMovesToEqualArrayElements.java
@@ -0,0 +1,10 @@
+package leetcode.minmovestomovearrayelements;
+
+public class MinMovesToEqualArrayElements {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
