0d699b0e5587934abc695891b75dee9a46e6afaa
Dwarak <dwaraknath.bakshi@gmail.com>
2017-04-24 07:37:41 -0700

    Making anagrams

---
+++ src/hackerrank/makinganagrams/MakingAnagrams.java
@@ -0,0 +1,90 @@
+package hackerrank.makinganagrams;
+
+import java.util.Scanner;
+
+/**
+ * Alice is taking a cryptography class and finding anagrams to be very useful.
+ * We consider two strings to be anagrams of each other if the first string's
+ * letters can be rearranged to form the second string. In other words, both
+ * strings must contain the same exact letters in the same exact frequency For
+ * example, bacdc and dcbac are anagrams, but bacdc and dcbad are not.
+ * 
+ * Alice decides on an encryption scheme involving two large strings where
+ * encryption is dependent on the minimum number of character deletions required
+ * to make the two strings anagrams. Can you help her find this number?
+ * 
+ * Given two strings, and , that may or may not be of the same length, determine
+ * the minimum number of character deletions required to make and anagrams. Any
+ * characters can be deleted from either of the strings.
+ * 
+ * This challenge is also available in the following translations:
+ * 
+ * Chinese Russian Input Format
+ * 
+ * The first line contains a single string, . The second line contains a single
+ * string, .
+ * 
+ * Constraints
+ * 
+ * It is guaranteed that and consist of lowercase English alphabetic letters
+ * (i.e., through ). Output Format
+ * 
+ * Print a single integer denoting the number of characters you must delete to
+ * make the two strings anagrams of each other.
+ * 
+ * Sample Input
+ * 
+ * cde abc Sample Output
+ * 
+ * 4 Explanation
+ * 
+ * We delete the following characters from our two strings to turn them into
+ * anagrams of each other:
+ * 
+ * Remove d and e from cde to get c. Remove a and b from abc to get c. We must
+ * delete characters to make both strings anagrams, so we print on a new line.
+ * 
+ * @author dwaraknathbs
+ *
+ */
+public class MakingAnagrams {
+
+	public static int numberNeeded(String first, String second) {
+		int[] alphabets = new int[26];
+		char[] fir = first.toCharArray();
+		/**
+		 * Keep a frequency count of each of the characters
+		 */
+		for (int i = 0; i < first.length(); i++) {
+			alphabets[fir[i] - 'a']++;
+		}
+
+		/**
+		 * Subtract the char count when seeing the same char in the next string
+		 */
+		char[] sec = second.toCharArray();
+		for (int i = 0; i < second.length(); i++) {
+			alphabets[sec[i] - 'a']--;
+		}
+
+		/**
+		 * Sweep the alphabet array and add the characters count which have
+		 * non-zero val
+		 */
+		int count = 0;
+		for (int i = 0; i < 26; i++) {
+			if (alphabets[i] != 0) {
+				count += Math.abs(alphabets[i]);
+			}
+		}
+		return count;
+	}
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		String a = in.next();
+		String b = in.next();
+		System.out.println(numberNeeded(a, b));
+	}
+
+}
