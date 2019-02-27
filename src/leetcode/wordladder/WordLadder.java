31c91f334f1dbebd2a5c111fe0201b4c6b84bd34
Dwarak <dwaraknath.bakshi@gmail.com>
2019-02-26 22:13:12 -0800

    Word Ladder

--- src/leetcode/wordladder/WordLadder.java
+++ src/leetcode/wordladder/WordLadder.java
@@ -6,6 +6,7 @@ import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
+import java.util.Set;
 
 /**
  * 
@@ -32,92 +33,62 @@ import java.util.Queue;
  *
  */
 public class WordLadder {
-	
-	 static class Pair {
-	        String word1;
-	        String word2;
-	        public Pair(String word1,String word2){
-	            this.word1=word1;
-	            this.word2=word2;
-	        }
-	    }
 
 	public static void main(String[] args) {
-		List<String> wordList= new ArrayList<>();
+		List<String> wordList = new ArrayList<>();
 		wordList.add("hot");
-		wordList.add("dog");
+		wordList.add("hog");
 		wordList.add("fog");
-		
+
 		System.out.println(ladderLength("hot", "fog", wordList));
 
 	}
-	
-	 public static int ladderLength(String beginWord, String endWord, List<String> wordList) {
-	        HashMap<String,String> backRef= new HashMap<>();
-	        Queue<Pair> wordLadder= new LinkedList<Pair>();
-	        HashSet<String> visited= new HashSet<String>();
-	        wordLadder.add(new Pair(beginWord,null));
-	        
-	        while(!wordLadder.isEmpty()){
-	            Pair current=wordLadder.remove();
-	            String word=current.word1;
-	            if(visited.contains(word)) continue;
-	            if(word.equals(endWord)) break;
-	            backRef.put(word,current.word2);
-	            visited.add(word);
-	           
-	            
-	            for(String neighbour: getNeighbours(word,wordList)){
-	                wordLadder.add(new Pair(neighbour,word));
-	            }
-	        }
-	        
-	        String curr=endWord;
-	        int count=0;
-	        while(!curr.equals(beginWord)){
-	            count++;
-	            curr=backRef.get(curr);
-	        }
-	        return count;
-	    }
-	    
-	 /**
-	  * Returns time limit exceeded
-	  * @param word
-	  * @param wordList
-	  * @return
-	  */
-	   static  List<String> getNeighboursold(String word,List<String> wordList){
-	        List<String> neighbours= new ArrayList<String>();
-	        char[] wordChars=word.toCharArray();
-	        for(String current: wordList){
-	            char[] currentChars=current.toCharArray();
-	            int count=0;
-	            for(int i=0; i<currentChars.length; i++){
-	                if(currentChars[i]!=wordChars[i]){
-	                    count++;
-	                }
-	                
-	            }
-	            if(count==1) neighbours.add(current);
-	        }
-	        return neighbours;
-	    }
-	   
-	static List<String> getNeighbours(String word, List<String> wordList) {
-		List<String> neighbours = new ArrayList<String>();
-		char[] wordChars = word.toCharArray();
-		for (int i = 0; i < wordChars.length; i++) {
-			for (char c = 'a'; c <= 'z'; c++) {
-				char old=wordChars[i];
-				wordChars[i] = c;
-				String str = new String(wordChars);
-				if (wordList.contains(str))
-					neighbours.add(str);
-				wordChars[i]=old;
-			}
+
+	static class Pair {
+		String word;
+		int level;
+
+		public Pair(String s, int l) {
+			this.word = s;
+			this.level = l;
 		}
-		return neighbours;
+	}
+
+	public static int ladderLength(String beginWord, String endWord, List<String> wordList) {
+
+		Set<String> words = new HashSet<>();
+		for (String s : wordList)
+			words.add(s);
+		Queue<Pair> queue = new LinkedList<>();
+		queue.add(new Pair(beginWord, 1));
+
+		while (!queue.isEmpty()) {
+			Pair p = queue.remove();
+			String curr = p.word;
+
+			int level = p.level;
+
+			if (curr.equals(endWord))
+				return level;
+
+			char[] current = curr.toCharArray();
+			for (int i = 0; i < current.length; i++) {
+				char tmp = current[i];
+				for (char c = 'a'; c <= 'z'; c++) {
+					current[i] = c;
+					String next = new String(current);
+					if (words.contains(next)) {
+						queue.add(new Pair(next, level + 1));
+						words.remove(next);
+					}
+				}
+				current[i] = tmp;
+			}
+
+		}
+
+		return 0;
+
 	}
 
 }
